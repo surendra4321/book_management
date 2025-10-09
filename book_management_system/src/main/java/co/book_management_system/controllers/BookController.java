2f2c3bec1e19b05ec9195dd51fc2533bdf0a5409
suren <surendskyrk870@gmail.com>
2025-10-10 04:56:56 +0530

    add the service layer

--- book_management_system/src/main/java/co/book_management_system/controllers/BookController.java
+++ book_management_system/src/main/java/co/book_management_system/controllers/BookController.java
@@ -1,21 +1,25 @@
 package co.book_management_system.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.book_management_system.entites.Book;
+import co.book_management_system.services.BookService;
 
 //@Controller
 @RestController
 public class BookController {
 	
+	@Autowired
+	private BookService bookService;
+	
 	//@RequestMapping(value="/books", method = RequestMethod.GET)
     @GetMapping("/books")
-	public Book getBook() {	
-    	Book book =new Book();
-    	book.setId(101);
-    	book.setTitle("Complete Java Programming");
-    	book.setAuthor("XYZ");
+	public List<Book> getBook() {	
+    	  List<Book> book= bookService.getAllBooks();
 		return book;
 	}
 
---
+++ book_management_system/src/main/java/co/book_management_system/services/BookService.java
@@ -0,0 +1,27 @@
+package co.book_management_system.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import co.book_management_system.entites.Book;
+
+@Service
+public class BookService {
+
+	private static List<Book> list = new ArrayList<>();
+
+	static {
+		list.add(new Book(101, "Full stack java programming", "xyz"));
+		list.add(new Book(102, "Full stack programming", "abc"));
+		list.add(new Book(103, "Java programming", "bcd"));
+		list.add(new Book(104, "All about React JS", "pqr"));
+	}
+
+	public List<Book> getAllBooks() {
+		return list;
+
+	}
+
+}
